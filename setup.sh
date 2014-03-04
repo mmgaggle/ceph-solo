5212acf84f24c2520a119875cf70fa0034970c07
Kyle Bader <kyle.bader@gmail.com>
2014-03-04 13:28:14 -0800

    cat eof + chef-solo call

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ sudo apt-get install -y git
 wget -O /tmp/chef_11.10.4-1.ubuntu.12.04_amd64.deb 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_11.10.4-1.ubuntu.12.04_amd64.deb'
 sudo dpkg -i /tmp/chef_11.10.4-1.ubuntu.12.04_amd64.deb
 sudo mkdir /etc/chef /var/chef-solo
-sudo echo <<EOF >> /tmp/solo.rb
+cat <<'EOF' > /tmp/solo.rb
 file_cache_path "/var/chef-solo"
 cookbook_path "/var/chef-solo/cookbooks"
 EOF
 sudo mv /tmp/solo.rb /etc/chef/solo.rb
 git clone https://github.com/mmgaggle/ceph-solo.git /tmp/ceph-solo.git
 sudo ln -s /tmp/ceph-solo.git/cookbooks /var/chef-solo/cookbooks
+sudo chef-solo -o 'recipe[ceph]'
