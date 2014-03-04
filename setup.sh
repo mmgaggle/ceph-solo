c452d53f463b0f0d5677d5d0c98b915d76965ec3
Kyle Bader <kyle.bader@gmail.com>
2014-03-04 13:20:19 -0800

    sudo make me a sandwich

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@ sudo apt-get install -y git
 wget -O /tmp/chef_11.10.4-1.ubuntu.12.04_amd64.deb 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_11.10.4-1.ubuntu.12.04_amd64.deb'
 sudo dpkg -i /tmp/chef_11.10.4-1.ubuntu.12.04_amd64.deb
 sudo mkdir /etc/chef /var/chef-solo
-sudo echo <<EOF >> /etc/chef/solo.rb
+sudo echo <<EOF >> /tmp/solo.rb
 file_cache_path "/var/chef-solo"
 cookbook_path "/var/chef-solo/cookbooks"
 EOF
+sudo mv /tmp/solo.rb /etc/chef/solo.rb
 git clone https://github.com/mmgaggle/ceph-solo.git /tmp/ceph-solo.git
 sudo ln -s /tmp/ceph-solo.git/cookbooks /var/chef-solo/cookbooks
