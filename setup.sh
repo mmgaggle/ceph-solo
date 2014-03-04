bee1146f1a57c9b8d7712bad0211c792a954ec78
Kyle Bader <kyle.bader@gmail.com>
2014-03-04 13:18:00 -0800

    setup cookbooks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install -y git
 wget -O /tmp/chef_11.10.4-1.ubuntu.12.04_amd64.deb 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_11.10.4-1.ubuntu.12.04_amd64.deb'
 sudo dpkg -i /tmp/chef_11.10.4-1.ubuntu.12.04_amd64.deb
 sudo mkdir /etc/chef /var/chef-solo
@@ -7,3 +8,5 @@ sudo echo <<EOF >> /etc/chef/solo.rb
 file_cache_path "/var/chef-solo"
 cookbook_path "/var/chef-solo/cookbooks"
 EOF
+git clone https://github.com/mmgaggle/ceph-solo.git /tmp/ceph-solo.git
+sudo ln -s /tmp/ceph-solo.git/cookbooks /var/chef-solo/cookbooks
