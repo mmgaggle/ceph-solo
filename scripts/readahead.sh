b2c68c0f12ef3ce25a7d7444f02fc6ea83b4828b
Kyle Bader <kyle.bader@gmail.com>
2014-04-03 10:11:39 -0700

    comment ssds out, echo for verification

--- scripts/readahead.sh
+++ scripts/readahead.sh
@@ -5,8 +5,10 @@ x=${first_osd_id}
 for DEVICE in $( grep 3907018584 /proc/partitions | awk '{print $4}' );do
   echo 1024 > /sys/block/${DEVICE}/queue/read_ahead_kb
   echo 256 > /sys/block/${DEVICE}/queue/nr_requests
+  echo -n "/dev/${DEVICE} read_ahead_kb: " && cat /sys/block/${DEVICE}/queue/read_ahead_kb
+  echo -n "/dev/${DEVICE} nr_requests: " && cat /sys/block/${DEVICE}/queue/nr_requests
 done
 
-for SSD in $( grep 195360984 /proc/partitions | awk '{print $4}' );do
+#for SSD in $( grep 195360984 /proc/partitions | awk '{print $4}' );do
   # add tuning for journal ssds?
-done
+#done
