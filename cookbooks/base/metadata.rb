0df5aa9546cf4820f61bd250a89137ddc812690d
Kyle Bader <kyle.bader@gmail.com>
2014-04-22 11:11:54 -0700

    rename tools to base, update bootstrap script

--- cookbooks/base/metadata.rb
+++ cookbooks/base/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Kyle Bader"
 maintainer_email "kyle.bader@inktank.com"
 license          "Apache 2.0"
-description      "Installs various observability and administration tools"
+description      "Base node setup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
