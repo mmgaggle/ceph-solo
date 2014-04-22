0a3e28ffa7d2c4d60033d3e9bde6f7c8224280f7
Kyle Bader <kyle.bader@gmail.com>
2014-04-21 19:41:56 -0700

    remove gluster stuff

--- cookbooks/gluster/recipes/dist_stripe_rep.rb
+++
@@ -1,9 +0,0 @@
-execute "create libvirt-pool volume" do
-  command "(sudo gluster peer status && echo \"Hostname: $(hostname -f)\") | grep -i hostname | cut -d: -f2 | xargs -n1 -I '{}' echo -n \"{}:/mnt/gluster/bricks/bricksv1 \""
-  not_if "gluster volume info libvirt-pool"
-end
-
-execute "start gluster volume" do
-  command "gluster volume start livbirt-pool" do
-  not_if "gluster volume info libvirt-pool"
-end
