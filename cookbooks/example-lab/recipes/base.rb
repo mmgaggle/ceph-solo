edf518423796bd1637bc84cec8024e1bbe707a30
Kyle Bader <kyle.bader@gmail.com>
2014-04-22 17:02:15 -0700

    typo

--- cookbooks/example-lab/recipes/base.rb
+++ cookbooks/example-lab/recipes/base.rb
@@ -2,7 +2,7 @@ include_recipe "ceph::deploy"
 
 ceph_deploy "install" do
   dev "--dev firefly"
-  nodes ["burnupi11","burnupi14","burnupi15","burnpui47","rhoda001","rhoda002","rhoda003","rhoda004"]
+  nodes ["burnupi11","burnupi14","burnupi15","burnupi47","rhoda001","rhoda002","rhoda003","rhoda004"]
   action :install
 end
 
